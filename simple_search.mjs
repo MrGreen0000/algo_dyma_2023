c1699d1353c09fa4ef43e5070c550ed37dfbb232
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-14 12:04:08 +0100

    update

--- simple_search.mjs
+++ simple_search.mjs
@@ -2,10 +2,14 @@ const tableau = new Array(100).fill().map((x, i) => i + 1);
 
 const numberToGuess = Math.floor(Math.random() * 100);
 
+let numberOffries = 0;
+
 console.log("numberToGuess :", numberToGuess);
 
 function simpleSearch(tableau, numberToGuess) {
+
     for (let i = 0; i < tableau.length; i++) {
+        numberOffries++;
         if (tableau[i] === numberToGuess) {
             return i;
         }
@@ -15,4 +19,6 @@ function simpleSearch(tableau, numberToGuess) {
 const result = simpleSearch(tableau, numberToGuess);
 
 console.log("result :", result);
+console.log("numberOffries :", numberOffries);
 console.log("result :", tableau[result]);
+
