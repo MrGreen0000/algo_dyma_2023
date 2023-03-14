9c34084ffab30036bba2ec42227719d70099f23f
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-14 21:12:14 +0100

    update

--- binary_search.mjs
+++ binary_search.mjs
@@ -1,31 +1,32 @@
-const tableau = new Array(100).fill().map((x, i) => i + 1);
+const tableau = new Array(100000000).fill().map((x, i) => i + 1);
 
-const numberToGuess = Math.floor(Math.random() * 100);
+const numberToGuess = Math.floor(Math.random() * 100000000);
 
 let numberOffries = 0;
 
 console.log("numberToGuess :", numberToGuess);
 
 function binarySearch(tableau, numberToGuess) {
-    let min = 0;
-    let max = tableau.length - 1;
-    while (min <= max) {
-        const mid = Math.floor((min + max) / 2);
-        const guess = tableau[mid];
-        numberOffries++;
-        if (guess === numberToGuess) {
-            return mid;
-        } else if (guess > numberToGuess) {
-            max = mid - 1;
-        } else {
-            min = mid + 1;
-        }
+  let min = 0;
+  let max = tableau.length - 1;
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    const guess = tableau[mid];
+    numberOffries++;
+    if (guess === numberToGuess) {
+      return mid;
+    } else if (guess > numberToGuess) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
     }
-    return null;
+  }
+  return null;
 }
-
+const start = performance.now();
 const result = binarySearch(tableau, numberToGuess);
-
+const end = performance.now();
+console.log("binarySearch time : ", end - start);
 console.log("result :", result);
 console.log("numberOffries :", numberOffries);
 console.log("result :", tableau[result]);
--- simple_search.mjs
+++ simple_search.mjs
@@ -1,24 +1,24 @@
-const tableau = new Array(100).fill().map((x, i) => i + 1);
+const tableau = new Array(100000000).fill().map((x, i) => i + 1);
 
-const numberToGuess = Math.floor(Math.random() * 100);
+const numberToGuess = Math.floor(Math.random() * 100000000);
 
 let numberOffries = 0;
 
 console.log("numberToGuess :", numberToGuess);
 
 function simpleSearch(tableau, numberToGuess) {
-
-    for (let i = 0; i < tableau.length; i++) {
-        numberOffries++;
-        if (tableau[i] === numberToGuess) {
-            return i;
-        }
+  for (let i = 0; i < tableau.length; i++) {
+    numberOffries++;
+    if (tableau[i] === numberToGuess) {
+      return i;
     }
+  }
 }
 
+const start = performance.now();
 const result = simpleSearch(tableau, numberToGuess);
-
+const end = performance.now();
+console.log("simpleSearch time : ", end - start);
 console.log("result :", result);
 console.log("numberOffries :", numberOffries);
 console.log("result :", tableau[result]);
-
